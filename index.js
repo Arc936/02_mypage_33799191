c6e6cb3bf4af81a64a42a1369c71f08d47de76b1
arc936 <blabb3337@gmail.com>
2025-10-16 23:10:43 +0100

    Html

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const port = 8000;
 http.createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World!");
-    
+    fs.readFile('index.html', function(err, data) {
+        if (err) {
+            // Handle error (e.g., file not found)
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            return res.end("404 File Not Found");
+        } 
+        
+        // 3. Set the Content-Type header to text/html
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // 4. Send the file content (data) as the response body
+        res.write(data); 
+        return res.end();
+    });
     
 }).listen(port, function() {
     console.log(`Node server is running on port ${port}...`);
